4cc2562165ddab2da6e05b850ae6b21a90b4d139
fsmi <15850562037@163.com>
2019-08-07 09:31:03 +0800

    init

---
+++ example/linkedList.py
@@ -0,0 +1,17 @@
+class Node
+    def __init__(data):
+        this.data = data
+        this.next = null
+
+    def setNext(node):
+        this.next = node
+
+class LinkedList
+    def __init__():
+        # 我们经常会给链表一个虚假的头，
+        this.root = new Node(null)
+        this.current = this.root.next
+    
+    def insert(data):
+        this.
+
