4e496f6c5173ed10defdbb590be0d80434bb8e28
fsmi <15850562037@163.com>
2019-08-12 23:41:23 +0800

    doublelinkedlist

--- example/DoubleLinkedList.py
+++ example/DoubleLinkedList.py
@@ -2,4 +2,62 @@ import DoublePointNode
 
 class DoubleLinkedList:
     def __init__(self):
-        self
+        # 也可以再多维护一个末尾节点，这样可以提高效率
+        # 比如说append就变成了O(1)的操作
+        # get也可以根据i的大小决定从尾还是头遍历
+        self.head = DoublePointNode.Node(None)
+        self.length = 0
+
+    def append(self, data):
+        self.insert(data, self.length)
+
+    def insert(self, data, i):
+        previous = self.get(i - 1)
+        try:
+            next = self.get(i + 1)
+        except IndexError:
+            next = None
+        current = DoublePointNode.Node(data)
+        previous.next = current
+        current.prev = previous
+        if next is not None:
+            next.prev = current
+            current.next = next
+        self.length += 1
+
+    def remove(self, i):
+        previous = self.get(i - 1)
+        try:
+            next = self.get(i + 1)
+        except IndexError:
+            next = None
+        current = previous.next
+        previous.next = next
+        if next is not None:
+            next.prev = previous
+        return current
+
+    def get(self, i):
+        if i < 0:
+            raise IndexError("Out of index")
+        current = self.head
+        while i >= 0:
+            current = current.next
+            if current is None:
+                raise IndexError("Out of index")
+            i -= 1
+        return current
+
+    class Iterator:
+        def __init__(self, list):
+            self.list = list
+            self.current = self.list.head
+
+        def __next__(self):
+            self.current = self.current.next
+            if self.current is None:
+                raise StopIteration()
+            return self.current
+
+    def __iter__(self):
+        return DoubleLinkedList.Iterator(self)
--- example/SingleLinkedList.py
+++ example/SingleLinkedList.py
@@ -5,6 +5,7 @@ class LinkedList:
     def __init__(self):
         # 我们经常会给链表一个虚假的头指针，因为头节点理论上没有前一个节点，所以假的指针可以保证头节点的操作和其他节点一样
         # 比如删除头节点这个时候就会用这个假的头节点指向第二个节点
+        # 这个我会在CircularList里举例
         self.head = SinglePointNode.Node(None)
         self.length = 0
 
@@ -14,7 +15,7 @@ class LinkedList:
     def insert(self, data, i):
         previous = self.get(i - 1)
         # 保存当前插入位置的节点
-        current = None if previous.next == None else previous.next
+        current = None if previous.next is None else previous.next
         # 让插入位置前一个的节点的next指针指向插入的节点
         previous.next = SinglePointNode.Node(data)
         # 让插入的节点的next指针指向
@@ -24,9 +25,9 @@ class LinkedList:
     def remove(self, i):
         previous = self.get(i - 1)
         # 保存删除节点的数据来返回
-        data = None if previous.next == None else previous.next.data
+        data = None if previous.next is None else previous.next.data
         # 前一个节点指向删除节点的后一个节点
-        previous.next = None if previous.next == None else previous.next.next
+        previous.next = None if previous.next is None else previous.next.next
         self.length -= 1
         return data
 
@@ -35,11 +36,13 @@ class LinkedList:
         node.data = data
 
     def get(self, i):
+        if i < 0:
+            raise IndexError("Out of index")
         current = self.head
         while i >= 0:
-            if current == None:
-                raise IndexError("Out of index")
             current = current.next
+            if current is None:
+                raise IndexError("Out of index")
             i -= 1
         return current
 
@@ -51,7 +54,7 @@ class LinkedList:
         # 迭代器实现__next__
         def __next__(self):
             self.current = self.current.next
-            if self.current == None:
+            if self.current is None:
                 raise StopIteration()
             return self.current
     
